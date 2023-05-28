eedd03e6b9e4323a24335a8ce31a4fc3c17299ef
Harun Biswas Rubel <harunbiswasrubel@gmail.com>
2023-05-28 20:06:39 +0600

    update

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -99,3 +99,39 @@ $(".slick-slider-2").slick({
     },
   ],
 });
+
+$(".slick-slider-3").slick({
+  centerMode: true,
+  centerPadding: "0px",
+  dots: false,
+  infinite: true,
+  arrows: true,
+  speed: 500,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
