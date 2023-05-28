64d9e8b9621490ea1cfa595914c324236e1bdae0
Harun Biswas Rubel <harunbiswasrubel@gmail.com>
2023-05-28 22:55:03 +0600

    update

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,3 +1,32 @@
+// header functionality
+const header = document.querySelector(".header");
+
+document.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    header.classList.add("scroll");
+  } else {
+    header.classList.remove("scroll");
+  }
+});
+// toggle
+const toggle = document.querySelector(".toggle");
+const close = document.querySelector(".close");
+const nav = document.querySelector(".nav");
+
+toggle.addEventListener("click", () => {
+  if (nav.classList.contains("show")) {
+    nav.classList.remove("show");
+  } else {
+    nav.classList.add("show");
+  }
+});
+
+close.addEventListener("click", () => {
+  if (nav.classList.contains("show")) {
+    nav.classList.remove("show");
+  }
+});
+
 $(".slick-slider").slick({
   dots: false,
   infinite: false,
@@ -9,10 +38,9 @@ $(".slick-slider").slick({
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 3,
+        slidesToShow: 2,
         slidesToScroll: 1,
         infinite: true,
-        dots: true,
       },
     },
     {
@@ -46,7 +74,6 @@ $(".slick-slider-1").slick({
         slidesToShow: 2,
         slidesToScroll: 1,
         infinite: true,
-        dots: true,
       },
     },
     {
@@ -77,10 +104,9 @@ $(".slick-slider-2").slick({
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 6,
+        slidesToShow: 4,
         slidesToScroll: 1,
         infinite: true,
-        dots: true,
       },
     },
     {
@@ -113,10 +139,9 @@ $(".slick-slider-3").slick({
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 5,
+        slidesToShow: 3,
         slidesToScroll: 1,
         infinite: true,
-        dots: true,
       },
     },
     {
